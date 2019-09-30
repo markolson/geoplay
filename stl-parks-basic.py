5404a0b7b6b8355a7772623487d928ab83a0fc7d
Mark Olson <theothermarkolson@gmail.com>
2019-09-30 16:30:55 -0500

    Simplify some

--- stl-parks-basic.py
+++ stl-parks-basic.py
@@ -1,6 +1,9 @@
+# Helpers
+import copy
 # Shapely **HAS** to be imported before anything else.
 from shapely.geometry import mapping, shape
 from shapely.prepared import prep
+
 # Project configuration code
 from kbrgeo.project import Project
 # Data sources
@@ -16,16 +19,30 @@ zcta = project.use(ZCTA)
 parks = project.use(StlParks)
 
 wanted_zips = zcta.filterTo(zipCodes=[63108, 63118, 63103, 63104, 63110])
-wanted_zips = helpers.addBufferArea(wanted_zips, buffer=0.001)
 
 for zip_area in wanted_zips:
-  touching_parks = helper.nearbyEnoughTo(zip_area, others=parks)
+  # puff the bounds of the zip codes out 0.001 arcradians
+  loose_zip_bounds = shape(zip_area['geometry']).buffer(0.001)
+
+  # Make a list of parks that touch our new expanded ZIP
+  touching_parks = list(filter(
+    lambda p: loose_zip_bounds.intersects(shape(p['geometry'])),
+    parks.shapefile()))
+
+  # Update the geometry in the shapefile
+  zip_area['geometry'] = mapping(loose_zip_bounds)
   # Write our variables
   zip_area['properties']['PARKCOUNT'] = len(touching_parks)
+  zip_area['properties']['PARKNAMES'] = ', '.join([p['properties']['TEXT_'] for p in touching_parks ])
 
 # Write out our final file
-buffered_zip = project.save_layer(
+project.save_layer(
   name='nearby-park-counts',
-  using_data=buffered_zips,
-  variables=['ZCTA5CE10','PARKCOUNT'])
+  using_data=wanted_zips,
+  variables={
+    'ZCTA5CE10': {'type': 'str', 'from_data': 'ZCTA5CE10'},
+    'PARKCOUNT': {'type': 'int', 'from_data': 'PARKCOUNT'},
+    'PARKNAMES': {'type': 'str', 'from_data': 'PARKNAMES'},
+  })
 
+project.save_csv(name='zip_parks', using_data=wanted_zips)
--- stl-parks-coverage.py
+++ stl-parks-coverage.py
@@ -28,18 +28,16 @@ project.save_layer(
     'ZCTA5CE10': {'type': 'str', 'from_data': 'ZCTA5CE10'},
   })
 
-# Do somethings that will be hidden away
-buffered_zips = copy.deepcopy(wanted_zips)
-[bounds.update({'geometry': mapping(shape(bounds['geometry']).buffer(0.001))}) for bounds in buffered_zips]
-
-for zip_area in buffered_zips:
-  zip_bounds = shape(zip_area['geometry'])
+for zip_area in wanted_zips:
+  loose_zip_bounds = shape(zip_area['geometry']).buffer(0.001)
 
   # Make a list of parks that touch our bulked out ZIP Code
   touching_parks = list(filter(
-    lambda p: zip_bounds.intersects(shape(p['geometry'])),
+    lambda p: loose_zip_bounds.intersects(shape(p['geometry'])),
     parks.shapefile()))
 
+  # Update the geometry in the shapefile
+  zip_area['geometry'] = mapping(loose_zip_bounds)
   # Write our variables
   zip_area['properties']['PARKCOUNT'] = len(touching_parks)
   zip_area['properties']['PARKNAMES'] = ', '.join([p['properties']['TEXT_'] for p in touching_parks ])
@@ -55,11 +53,11 @@ for zip_area in buffered_zips:
 # Write out our final file
 project.save_layer(
   name='nearby-park-counts',
-  using_data=buffered_zips,
+  using_data=wanted_zips,
   variables={
     'ZCTA5CE10': {'type': 'str', 'from_data': 'ZCTA5CE10'},
     'PARKCOUNT': {'type': 'int', 'from_data': 'PARKCOUNT'},
     'PARKNAMES': {'type': 'str', 'from_data': 'PARKNAMES'},
   })
 
-project.save_csv(name='zip_parks', using_data=buffered_zips)
+project.save_csv(name='zip_parks', using_data=wanted_zips)
